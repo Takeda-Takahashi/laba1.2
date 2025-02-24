d00611ceebceec3f5225d4bd9d9f431e1b423ad0
Vika <tori.zolina@gmail.com>
2025-02-25 00:19:25 +0300

    Код оформлен в виде функции, которая вызывается 17 раз

--- laba1.2/laba1.2.py
+++ laba1.2/laba1.2.py
@@ -1,7 +1,10 @@
 ﻿import random
 
-# Генерируем список из 17 случайных чисел от 5 до 700
-random_list = [random.randint(5, 700) for _ in range(17)]
+def generate_random_list(num_elements):
+    """Генерирует список случайных чисел от 5 до 700 с указанным числом элементов"""
+    return [random.randint(5, 700) for _ in range(num_elements)]
+
+# Вызов функции с 17 элементами
+random_list = generate_random_list(17)
 
 print(random_list)
-
